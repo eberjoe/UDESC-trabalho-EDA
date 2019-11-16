a902861ab70959c625cfb545521eb0ffe81b69c4
eberjoe <eberjoe@gmail.com>
2019-11-16 19:18:02 -0300

    Add funcao para verificar disjuncao

--- main.c
+++ main.c
@@ -1,30 +1,36 @@
+#include <stdio.h>
 #include "lista.h"
 
 void mostra_float(void *x);
 int compara_float(void *x, void *y);
+int disjuntos(Conjuntos c);
 
 int main() {
-    int lin, col, i, j, x;
-    printf("Digite o numero de linhas e o numero de colunas: ");
-    scanf("%d %d", &lin, &col);
+    int lin, i, j;
+    float x;
+    printf("Digite o numero de conjuntos: ");
+    scanf("%d", &lin);
     Conjuntos m, sub;
 
     inicializa_conjuntos(&m, sizeof(Conjuntos));
-    for (i = 0; i < lin; i++) {
-        inicializa_conjuntos(&sub, sizeof(int));
-        for (j = 0; j < col; j++) {
-            printf("Entre um valor para a coordenada %d, %d: ", i, j);
-            scanf("%d", &x);
-            insereNoFim(&sub, &x);
-        }
+    for (i = 1; i <= lin; i++) {
+        inicializa_conjuntos(&sub, sizeof(float));
+        printf("Entre um elemento representante 'float' para o conjunto %d: ", i);
+        scanf("%f", &x);
+        insereNoInicio(&sub, &x);
         insereNoFim(&m, &sub);
+        if (!disjuntos(m)) {
+            printf("Os conjuntos nao sao disjuntos! Os elementos devem ser diferentes!");
+            return 1;
+        }
     }
+
     printf("Dados da matriz:\n");
     for (i = 0; i < lin; i++) {
         leNaPos(&m, &sub, i);
-        for (j = 0; j < col; j++) {
+        for (j = 0; j < sub.qtd; j++) {
             leNaPos(&sub, &x, j);
-            printf("%d ", x);
+            printf("%f ", x);
         }
         printf("\n");
     }
@@ -49,3 +55,25 @@ int compara_float(void *x, void *y) {
     return 0;
 }
 
+int disjuntos(Conjuntos c) {
+    if (c.qtd < 2)
+        return 1;
+    int i, j, k, l;
+    float x, y;
+    Conjuntos sub, sub2;
+    for (i = 0; i < c.qtd-1; i++) {
+        leNaPos(&c, &sub, i);
+        for (j = 0; j < sub.qtd; j++) {
+            leNaPos(&sub, &x, j);
+            for (k = i+1; k < c.qtd; k++) {
+                leNaPos(&c, &sub2, k);
+                for (l = 0; l < sub2.qtd; l++) {
+                    leNaPos(&sub2, &y, l);
+                    if (!compara_float(&x, &y))
+                        return 0;
+                }
+            }
+        }
+    }
+    return 1; // sao disjuntos
+}
